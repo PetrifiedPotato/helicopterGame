70a16a9789d9fadd9e94ee8e04074b391bef2ac1
PetrifiedPotato <73384879+PetrifiedPotato@users.noreply.github.com>
2021-02-13 21:25:39 +0530

    Add files via upload

---
+++ SupplyMission-master/sketch.js
@@ -0,0 +1,79 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground,side1,side2,floor
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+	side1=createSprite(425,630,14,50);
+	side2=createSprite(325,630,14,50);
+	floor=createSprite(380,660,123,14)
+
+
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.2, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+  //side1.position.x = mouse.x
+  //side1.x=mouse.x
+  console.log(side1.x)
+  console.log(side1.y)
+ 
+}
+
+function keyPressed() {
+ if (keyCode === 32) {
+   
+		
+	Matter.Body.setStatic(packageBody,false);
+		}
+		
+		
+  
+}
+
+
+
